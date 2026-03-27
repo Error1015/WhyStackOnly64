a8250d8d53a629bad786b9815306130dc63f5ece
Error1015 <156535541+Error1015@users.noreply.github.com>
2026-03-27 18:11:09 +0800

    添加黑名单功能，管理生效物品的范围。并更新版本号到1.1.0

--- src/main/java/org/error1015/wso16reborn/Config.java
+++ src/main/java/org/error1015/wso16reborn/Config.java
@@ -2,15 +2,27 @@ package org.error1015.wso16reborn;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
     public static final ModConfigSpec SPEC;
 
-    public static final ModConfigSpec.BooleanValue notOnlyStackOf16 = BUILDER.define("not_only_stack_of_16", false);
+    public static final ModConfigSpec.BooleanValue IsMatchAll = BUILDER.define("not_only_stack_of_16", false);
     public static final ModConfigSpec.IntValue size = BUILDER.defineInRange("size", 64, 1, 99);
 
     public static final ModConfigSpec.BooleanValue potions = BUILDER.define("potions", false);
     public static final ModConfigSpec.BooleanValue food = BUILDER.define("food", false);
+
+    // 存储的字符串格式需要满足: modid:namespace
+    public static final ModConfigSpec.ConfigValue<List<? extends String>> blackNameList = BUILDER
+            .comment("Require modid:item_name","Such as [blackNameList = [\"minecraft:diamond\", \"minecraft:dirt\", ...]]")
+            .defineList(
+                "blackNameList",
+                Collections.emptyList(),
+                s -> s instanceof String);
 
     static {
         SPEC = BUILDER.build();
--- src/main/java/org/error1015/wso16reborn/Wso16reborn.java
+++ src/main/java/org/error1015/wso16reborn/Wso16reborn.java
@@ -2,16 +2,21 @@ package org.error1015.wso16reborn;
 
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.item.*;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PotionItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.event.ModifyDefaultComponentsEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(Wso16reborn.MODID)
 public class Wso16reborn {
     public static final String MODID = "wso64";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     public Wso16reborn(IEventBus modEventBus, ModContainer modContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -19,7 +24,7 @@ public class Wso16reborn {
     }
 
     public void setComponentsForItem(ModifyDefaultComponentsEvent event) {
-        if (Config.notOnlyStackOf16.get()) {
+        if (Config.IsMatchAll.get()) {
             event.modifyMatching(this::matchAll, this::setMaxSize);
         } else {
             event.modifyMatching(this::match16, this::setMaxSize);
@@ -34,7 +39,20 @@ public class Wso16reborn {
         boolean isFood = Config.food.get() && stack.getFoodProperties(null) != null;
         boolean isPotion = Config.potions.get() && item instanceof PotionItem;
 
-        return maxSizeNoOne || isFood || isPotion;
+        boolean b = maxSizeNoOne || isFood || isPotion;
+        String itemKey = BuiltInRegistries.ITEM.getKey(item).toString();
+
+        for (var blackKey : Config.blackNameList.get()) {
+            int colonIndex = blackKey.indexOf(':');
+            if (colonIndex != -1) {
+                    if (itemKey.equals(blackKey)) {
+                    return false;   // 物品在黑名单中，排除
+                }
+            } else {
+                LOGGER.warn("{} is not valid content.", blackKey);
+            }
+        }
+        return b;
     }
 
     private boolean match16(Item item) {
